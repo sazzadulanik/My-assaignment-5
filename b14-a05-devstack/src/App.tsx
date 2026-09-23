a0b95b5f52373a44a47f208c176c30ef5e287abd
sazzadulanik <sazzadulhasananik2@gmail.com>
2026-09-24 00:27:41 +0600

    Add banner section

--- b14-a05-devstack/src/App.tsx
+++ b14-a05-devstack/src/App.tsx
@@ -1,10 +1,11 @@
 import Nav from "./Components/Nav";
-
+import Banner from "./Components/banner";
 
 function App() {
   return (
     <>
       <Nav />
+      <Banner />
     
     </>
   );
---
+++ b14-a05-devstack/src/Components/banner.tsx
@@ -0,0 +1,44 @@
+const Banner = () => {
+  return (
+    <section className="bg-white">
+      <div className="container mx-auto px-6 py-16">
+        <div className="grid items-center gap-10 lg:grid-cols-2">
+
+          {/* Left Content */}
+          <div>
+            <h1 className="text-5xl font-bold leading-tight text-gray-900">
+              Build Your Ideal{" "}
+              <span className="bg-gradient-to-r from-red-500 to-purple-600 bg-clip-text text-transparent">
+                Development Stack
+              </span>
+            </h1>
+
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Explore frontend, backend, database, and tooling options,
+              compare them side by side, and build the stack that fits your
+              next project.
+            </p>
+
+            <div className="mt-8 flex gap-4">
+              <button className="rounded-lg bg-gradient-to-r from-red-500 to-purple-500 px-6 py-3 font-semibold text-white">
+                Explore Technologies
+              </button>
+
+              <button className="rounded-lg border-1 border-gray-400 px-6 py-3 font-semibold text-black">
+                Learn More
+              </button>
+            </div>
+          </div>
+
+          <div>
+            <img
+              src="/assets/banner-stack.png" alt="" />
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
